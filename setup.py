97daea91a9025afbb7be554f8dd87c3f7e9ad641
Rory McCann <rory@technomancy.org>
2012-04-07 19:21:48 +0100

    manually added bingbot

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="robot-detection",
-      version="0.1",
+      version="0.2",
       author="Rory McCann",
       author_email="rory@technomancy.org",
       py_modules=['robot_detection'],
